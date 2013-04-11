311bafe851b873e7c336df055ce8c065bb45f849
andrewkeig <andrew.keig@gmail.com>
2013-04-11 23:49:18 +0100

    adding tests

--- test/test.js
+++ test/test.js
@@ -3,102 +3,101 @@ var Api = require('../lib/api');
 
 
 describe("Monitor Api", function() {
+   describe("When we do not provide options to api", function() {
+    var api, error;
 
-  describe.only("When invalid url", function() {
+    try { 
+      api = new Api();
+    } catch (err) {
+      error = err
+    }
 
-    before(function(done) {
-      var actualurl = "http://www.airasoul.net/missing/";
+    it("should contain correct retry value", function() {
+        error.toString().should.include("Error: Please provide options");
+    });
+  });
 
-      var options = {
+  describe("When we do not provide option to api for retry", function() {
+    var options = {
+        timeout:1,
+        minutes_between_notification:1
+    }
+
+    var api, error;
+
+    try { 
+      api = new Api(options);
+    } catch (err) {
+      error = err
+    }
+
+    it("should throw error", function() {
+        error.toString().should.include("Error: Please provide a retry value");
+    });
+  });
+
+  describe("When we provide options to api", function() {
+    var options = {
         retries:2,
         timeout:1,
         minutes_between_notification:1
-      }
+    }
 
-      var api = new Api(options);
+    var api = new Api(options);
+
+    it("should contain correct retry value", function() {
+        api.retries.should.equal(2);
     });
 
-    it("should validate", function(done) {
-      this.timeout(0);
-      api.monitor(actualurl);
-      api.on('snitch', function(url) {
-        console.log("snitch");
-        url.should.equal(actualurl);
-        this =null;
-        done();
-      });
-    });
-  });
-
-  describe("When invalid url and empty options", function() {
-
-    before(function(done) {
-      var actualurl = "http://www.airasoul.net/missing/";
-      var api = new Api();
+    it("should contain correct timeout value", function() {
+        api.timeout.should.equal(1);
     });
 
-    it("should validate", function(done) {
-      this.timeout(0);
-      api.monitor(actualurl);
-      //api.on('snitch', function(url) {
-        console.log("snitch");
-        url.should.equal(actualurl);
-      //  this =null;
-      //  done();
-      //});
+    it("should contain correct minutes between notification value", function() {
+        api.timeout.should.equal(1);
     });
   });
 
-  describe("When multiple urls and one is invalid", function() {
+  describe("When single invalid url", function() {
+    it("should snitch once", function(done) {
 
-    before(function(done) {
-      var urlOne = "http://www.airasoul.net/missing/";
-      var urlTwo = "http://www.airasoul.net";
+      var actualurl = "http://www.airasoul.net/missing/1";
 
       var options = {
-        retries:3,
-        timeout:1,
-        minutes_between_notification:1
+          retries:2,
+          timeout:1,
+          minutes_between_notification:1
       }
 
       var api = new Api(options);
-    });
+      api.monitor(actualurl);
 
-    it("should validate", function(done) {
-      api.monitor(urlOne);
-      api.monitor(urlTwo);
       api.on('snitch', function(url) {
-        url.should.equal();
+        url.should.equal(actualurl);
         done();
       });
     });
   });
 
-   describe("When empty url", function() {
+  describe("When multiple urls; one invalid", function() {
+    it("should snitch once", function(done) {
+      var actualurl = "http://www.airasoul.net/missing/3";
+      var goodurl = "http://www.airasoul.net";
 
-    before(function(done) {
-      var urlOne = "";
+      var options = {
+          retries:2,
+          timeout:1,
+          minutes_between_notification:1
+      }
 
-      var api = new Api();
-    });
+      var api = new Api(options);
+      api.monitor(actualurl);
+      api.monitor(goodurl);
 
-    it("should validate", function() {
-      api.monitor(urlOne);
-      should.throw.error;
-    });
-  });
-
-   describe("When invalid url", function() {
-
-    before(function(done) {
-      var urlOne = "";
-
-      var api = new Api();
-    });
-
-    it("should validate", function() {
-      api.monitor(urlOne);
-      should.throw.error;
+      api.on('snitch', function(url) {
+        url.should.equal(actualurl);
+        done();
+      });
     });
   });
 });
