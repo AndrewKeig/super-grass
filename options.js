dfc813ff315f64663610d472e7913de0c59e3660
Andrew Keig <andrew.keig@gmail.com>
2013-06-03 18:26:21 +0100

    fixing

--- options.js
+++ options.js
@@ -1,8 +1,8 @@
 module.exports = {
   settings: {
-    interval: "1000",
-    retry: "3",
-    retryTimeout: "500",
+    interval: "10000",
+    retry: "5",
+    retryTimeout: "1000",
     email: {
       login: "you@somewhere.com",
       password: "password",
@@ -23,29 +23,6 @@ module.exports = {
     name: "api for blog.airasoul.net",
     type: "api",
     host: "http://blog.airasoul.net",
-    enabled : true
-  },
-  { 
-    name: "local mongo",
-    type: "mongo",
-    host: "localhost",
-    database: "staging",
-    port: 27017,
-    enabled : true
-  },
-  {
-    name: "redis local",
-    type: "redis",
-    host: "http://127.0.0.1",
-    port: 6379,
-    enabled : true
-  },
-  {
-    name: "local rabbitmq",
-    type: "basicauth", 
-    host: "http://127.0.0.1:15672/api/aliveness-test/%2F",
-    username: "guest",
-    password: "guest",
     enabled : true
   }]
 }
